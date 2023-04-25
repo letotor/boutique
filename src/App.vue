c1c18ac44965a03610503c123d44996020f6425b
victor DG <v.degermain@gmail.com>
2023-04-25 23:48:05 +0200

    last commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div class="app-container" :class="{ gridEmpty: cartEmpty }">
     <TheHeader class="header" />
 
-    <p @update-filter="updateFilter">test :{{ displayTest }}</p>
     <Shop
       class="shop"
       @update-filter="updateFilter"
