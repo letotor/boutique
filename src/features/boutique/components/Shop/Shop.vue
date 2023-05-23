52a62710befcc4ef587819ba78586f591437400a
victor DG <v.degermain@gmail.com>
2023-05-23 16:56:20 +0200

    add scrollbar on shop

--- src/features/boutique/components/Shop/Shop.vue
+++ src/features/boutique/components/Shop/Shop.vue
@@ -7,7 +7,7 @@
       class="shop-filter m-20"
     />
     <ShopProductList
-      class="flex-fill"
+      class="flex-fill scrollable"
       @add-product-to-cart="emit('addProductToCart', $event)"
       :products="products"
     />
@@ -38,6 +38,24 @@ const emit = defineEmits<{
 </script>
 
 <style lang="scss" scoped>
+.scrollable {
+  overflow-y: auto;
+  overflow-x: hidden;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  height: calc(100vh - 96px);
+  //scrollbar-color: var(--primary-1);
+  &::-webkit-scrollbar {
+    background-color: var(--gray-1);
+    opacity: 0.8;
+    color: grey;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--gray-2);
+    
+    border-radius: 10px;
+  }
+}
 .shop-filter {
   flex: 0 0 200px;
 }
