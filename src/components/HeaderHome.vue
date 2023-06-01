ef1ef4ad6090a8cef6fdba45fc33cc94eb701435
victor DG <v.degermain@gmail.com>
2023-06-01 17:39:01 +0200

    add burger to responsive on right screen

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -4,30 +4,32 @@
       <img src="../assets/images/Logo_fusee_un_peu_penchee.png" alt="logo" />
       <span class="logo">DGweb</span>
     </a>
-
-    <ul class="d-flex flex-row align-items-center flex-fill">
-      <li class="mr-20">
-        <pre>page :{{ props.page }}</pre>
-        <a
-          :class="{ actives: page === 'boutique' }"
-          @click="emit('navigate', 'boutique')"
-          href=" #"
-          >Boutique</a
-        >
-      </li>
-      <li class="mr-10">
-        <a
-          :class="{ actives: page === 'admin' }"
-          @click="emit('navigate', 'admin')"
-          href=" #"
-          >Admin</a
-        >
-      </li>
-    </ul>
-    <ul class="d-flex flex-row align-items-center">
-      <li class="mr-20"><a href="#"> Inscription </a></li>
-      <li><a href="#"> Connexion </a></li>
-    </ul>
+    <div class="d-flex flex-row align-items-center flex-fill actions-container">
+      <ul class="d-flex flex-row align-items-center flex-fill hide-xs">
+        <li class="mr-20">
+          <pre>page :{{ props.page }}</pre>
+          <a
+            :class="{ actives: page === 'boutique' }"
+            @click="emit('navigate', 'boutique')"
+            href=" #"
+            >Boutique</a
+          >
+        </li>
+        <li class="mr-10">
+          <a
+            :class="{ actives: page === 'admin' }"
+            @click="emit('navigate', 'admin')"
+            href=" #"
+            >Admin</a
+          >
+        </li>
+      </ul>
+      <ul class="d-flex flex-row hide-xs">
+        <li class="mr-20"><a href="#"> Inscription </a></li>
+        <li><a href="#"> Connexion </a></li>
+      </ul>
+      <i class="fa-solid fa-bars show-xs" />
+    </div>
   </header>
 </template>
 
@@ -41,6 +43,7 @@ console.log('HeaderHome', props?.page);
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/scss/mixins';
 header {
   background-color: var(--primary-1);
   a {
@@ -60,6 +63,18 @@ header {
     text-decoration: underline;
     background-color: red;
     color: yellow;
+  }
+  i {
+    color: white;
+
+    @include mixins.sm {
+      display: none;
+    }
+  }
+  .actions-container {
+    @include mixins.xs {
+      justify-content: flex-end;
+    }
   }
 }
 </style>
